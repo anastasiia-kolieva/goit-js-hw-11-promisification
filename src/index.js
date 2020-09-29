22fcaf918d0a5f907d3488b46ffd6bb27a6579b3
anastasiia-kolieva <nastya1110@gmail.com>
2020-09-29 20:44:40 +0300

    made second and third task of promisification

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // Task 1
 
+const { resolve, reject } = require('core-js/fn/promise');
+
 // Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд. Значением исполнившегося промиса должно быть то кол-во миллисекунд которое передали во время вызова функции delay.
 
 const delay = ms => {
@@ -16,3 +18,67 @@ const logger = time => console.log(`Resolved after ${time}ms`);
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
+
+// Task 2
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    resolve(updatedUsers);
+  });
+};
+const loggerNew = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(loggerNew);
+toggleUserState(users, 'Lux').then(loggerNew);
+
+// Task 3
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = transaction => {
+  return new Promise((resolve, reject) => {
+    const delay = randomIntegerFromInterval(200, 500);
+
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve(transaction);
+      } else {
+        reject(transaction);
+      }
+    }, delay);
+  });
+};
+
+const logSuccess = transaction => {
+  console.log(
+    `Transaction ${transaction.id} processed in ${transaction.amount}ms`,
+  );
+};
+
+const logError = transaction => {
+  console.warn(
+    `Error processing transaction ${transaction.id}. Please try again later.`,
+  );
+};
+
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
